d34900d2c4fc49fb3b405733886d68b4a2ccc5f3
diegocaceres21 <diego.caceres@ucb.edu.bo>
2023-08-23 18:04:42 -0400

    tabla

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HorariosModule } from './horarios/horarios.module';
 
 @Module({
-  imports: [],
+  imports: [HorariosModule],
   controllers: [AppController],
   providers: [AppService],
 })
